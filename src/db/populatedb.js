58c1426b800afbd52b2c7921a3eb8974690c0916
Dax Davis <daxantz@hotmail.com>
2024-09-02 20:20:12 -0400

    created database files

---
+++ src/db/populatedb.js
@@ -0,0 +1,79 @@
+require("dotenv").config({ path: "../../.env" });
+const { Client } = require("pg");
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_PORT:", process.env.DB_PORT);
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS author (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR (255) NOT NULL,
+  last_name VARCHAR (255) NOT NULL,
+  birth_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS publisher (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR ( 255 ) NOT NULL,
+  date_established Date
+);
+CREATE TABLE IF NOT EXISTS manga (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR ( 255 ) NOT NULL,
+  release_date Date,
+  publisher_id INTEGER,
+  author_id INTEGER,
+  FOREIGN KEY (publisher_id) REFERENCES publisher(id),
+  FOREIGN KEY (author_id) REFERENCES author(id)
+
+);
+CREATE TABLE IF NOT EXISTS genre (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  type VARCHAR (255) NOT NULL
+);
+
+
+
+CREATE TABLE IF NOT EXISTS manga_genre (
+  manga_id INTEGER,
+  genre_id INTEGER,
+  FOREIGN KEY (manga_id) REFERENCES manga(id),
+  FOREIGN KEY (genre_id) REFERENCES genre(id),
+  PRIMARY KEY (manga_id, genre_id)
+);
+
+
+INSERT INTO author (first_name, last_name, birth_date)
+VALUES ('masashi', 'kishimoto', '1974-11-08');
+
+INSERT INTO publisher (name, date_established)
+VALUES
+  ('shueisha', '1926-08-08');
+
+INSERT INTO manga (title, release_date, publisher_id, author_id)
+VALUES
+  ('naruto', '1999-09-14', (SELECT id FROM publisher WHERE name = 'shueisha'), (SELECT id FROM author WHERE first_name = 'masashi' AND last_name = 'kishimoto'));
+
+INSERT INTO genre (type)
+VALUES ('adventure');
+
+INSERT INTO manga_genre (manga_id, genre_id)
+VALUES ((SELECT id FROM manga WHERE title = 'naruto'), (SELECT id FROM genre WHERE type = 'adventure'))
+
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
---
+++ src/db/queries.js
@@ -0,0 +1,39 @@
+const pool = require("./pool");
+
+async function insertManga(title, releaseDate, publisher, author) {
+  await pool.query(
+    "INSERT INTO manga (title, release_date, publisher_id, author_id) VALUES ($1, $2, $3, $4)",
+    [title, releaseDate, publisher, author]
+  );
+}
+async function insertAuthor(firstName, lastName, birthDate) {
+  await pool.query(
+    "INSERT INTO author (first_name, last_name, birth_date) VALUES ($1, $2, $3)",
+    [firstName, lastName, birthDate]
+  );
+}
+
+async function insertPublisher(name, dateEstablished) {
+  await pool.query(
+    "INSERT INTO publisher (name, date_established) VALUES ($1, $2)",
+    [name, dateEstablished]
+  );
+}
+
+async function getAllAuthors() {
+  const { rows } = await pool.query("SELECT * FROM author");
+  return rows;
+}
+
+async function getAllPublishers() {
+  const { rows } = await pool.query("SELECT * FROM publisher");
+  return rows;
+}
+
+module.exports = {
+  insertManga,
+  insertAuthor,
+  insertPublisher,
+  getAllAuthors,
+  getAllPublishers,
+};
