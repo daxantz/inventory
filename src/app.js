b0a561b1df2d55cb45d39dd8d067a2a738925a2a
Dax Davis <daxantz@hotmail.com>
2024-09-02 20:20:57 -0400

    imported routers for app to use

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-const express = require("express");
+require("dotenv").config();
 
+const express = require("express");
+const path = require("path");
 const app = express();
+
+const mangaRouter = require("../src/routes/mangaRouter");
+const authorRouter = require("./routes/authorRouter");
+const publisherRouter = require("./routes/publisherRouter");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("it works ");
 });
+
+app.use(mangaRouter);
+app.use(authorRouter);
+app.use(publisherRouter);
 
 app.listen(3000, (req, res) => {
   console.log("Server running on port 3000");
