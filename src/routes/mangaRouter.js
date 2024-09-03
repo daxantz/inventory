79efb3326ec30497a6d1486cbee60e0aee00aff2
Dax Davis <daxantz@hotmail.com>
2024-09-02 20:42:01 -0400

    added router to show all manga

--- src/routes/mangaRouter.js
+++ src/routes/mangaRouter.js
@@ -2,9 +2,9 @@ const { Router } = require("express");
 const mangaController = require("../controllers/mangaController"); // insert path to manga controller
 const mangaRouter = Router();
 
-// mangaRouter.get("/", mangaController.mangaListGet); // get all manga
+mangaRouter.get("/manga", mangaController.mangaListGet); // get all manga
 // mangaRouter.get("/manga/:id", mangaController.mangaGet); // get specific manga
-mangaRouter.get("/manga", mangaController.mangaCreateGet); //get the manga creation form
+mangaRouter.get("/manga/create", mangaController.mangaCreateGet); //get the manga creation form
 mangaRouter.post("/manga", mangaController.mangaCreatePost); // create a new manga entry
 // mangaRouter.put("/manga/:id", mangaController.mangaUpdatePut); // update a single manga
 // mangaRouter.delete("/manga/:id", mangaController.mangaDelete); // delete a single manga
