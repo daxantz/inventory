8846716f54b917758ca7bda7292ef690426e236b
Dax Davis <daxantz@hotmail.com>
2024-09-03 18:11:50 -0400

    added update routes

--- src/routes/mangaRouter.js
+++ src/routes/mangaRouter.js
@@ -3,10 +3,11 @@ const mangaController = require("../controllers/mangaController"); // insert pat
 const mangaRouter = Router();
 
 mangaRouter.get("/manga", mangaController.mangaListGet); // get all manga
-// mangaRouter.get("/manga/:id", mangaController.mangaGet); // get specific manga
-mangaRouter.get("/manga/create", mangaController.mangaCreateGet); //get the manga creation form
+mangaRouter.get("/manga/:id", mangaController.mangaGet); // get specific manga
+mangaRouter.get("/add-manga", mangaController.mangaCreateGet); //get the manga creation form
 mangaRouter.post("/manga", mangaController.mangaCreatePost); // create a new manga entry
-// mangaRouter.put("/manga/:id", mangaController.mangaUpdatePut); // update a single manga
+mangaRouter.get("/manga/:id/edit", mangaController.updateFormGet); // update a single manga
+mangaRouter.post("/manga/:id", mangaController.mangaUpdatePut); // update a single manga
 // mangaRouter.delete("/manga/:id", mangaController.mangaDelete); // delete a single manga
 
 module.exports = mangaRouter;
