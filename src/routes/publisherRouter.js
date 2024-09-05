59cddcf160994b3de5b2ca2639eba5a691fc9783
Dax Davis <daxantz@hotmail.com>
2024-09-04 21:38:10 -0400

    added route to get all publishers and edited the publisher form get url to a better name

--- src/routes/publisherRouter.js
+++ src/routes/publisherRouter.js
@@ -2,9 +2,9 @@ const { Router } = require("express");
 const publisherController = require("../controllers/publisherController"); // insert path to publisher controller
 const publisherRouter = Router();
 
-// publisherRouter.get("/", publisherController.publisherListGet); // get all publisher
+publisherRouter.get("/publishers", publisherController.publisherListGet); // get all publisher
 // publisherRouter.get("/publisher/:id", publisherController.publisherGet); // get specific publisher
-publisherRouter.get("/publisher", publisherController.publisherCreateGet); //get the publisher creation form
+publisherRouter.get("/add-publisher", publisherController.publisherCreateGet); //get the publisher creation form
 publisherRouter.post("/publisher", publisherController.publisherCreatePost); // create a new publisher entry
 // publisherRouter.put("/publisher/:id", publisherController.publisherUpdatePut); // update a single publisher
 // publisherRouter.delete("/publisher/:id", publisherController.publisherDelete); // delete a single publisher
