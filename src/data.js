313cfb36ab839a4801b5ad30dbbaec254b4434fb
Dax Davis <daxantz@hotmail.com>
2024-09-04 20:44:19 -0400

    links for nav bar

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const links = [
+  { href: "/", text: "Home" },
+  { href: "/author", text: "Author" },
+  { href: "/manga", text: "Manga" },
+  { href: "/publisher", text: "Publisher" },
+];
+
+module.exports = {
+  links,
+};
