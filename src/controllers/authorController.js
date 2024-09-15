9c3bcd994867d9a05e1a25663bc99692bc34cc12
daxantz <daxantz@gmail.com>
2024-09-15 12:48:24 -0400

    created handlers to get a list of all authors and a handler to get a single author

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -3,6 +3,30 @@ const data = require("../data.js");
 exports.authorCreateGet = (req, res) => {
   res.render("authorCreateForm", { links: data.links });
 };
+exports.authorListGet = async (req, res) => {
+  try {
+    const authors = await db.getAllAuthors();
+    if (!authors) {
+      res.status(404).send("no authors currently exist");
+    }
+    res.render("allAuthors", { authors: authors, links: data.links });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("server error");
+  }
+};
+
+exports.authorGet = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const author = await db.getAuthor(Number(id));
+    console.log(author);
+    res.render("authorDetails", { author: author, links: data.links });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("server error");
+  }
+};
 
 exports.authorCreatePost = async (req, res) => {
   console.log(req.body);
