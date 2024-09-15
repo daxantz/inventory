3af455361cdabfd308824e29c3100eb579b83376
daxantz <daxantz@gmail.com>
2024-09-15 12:16:32 -0400

    home path now renders a home page

--- src/app.js
+++ src/app.js
@@ -2,23 +2,27 @@ require("dotenv").config();
 
 const express = require("express");
 const path = require("path");
+const db = require("./db/queries");
 const app = express();
-
+const methodOverride = require("method-override");
+const data = require("./data.js");
 const mangaRouter = require("../src/routes/mangaRouter");
 const authorRouter = require("./routes/authorRouter");
 const publisherRouter = require("./routes/publisherRouter");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
-  res.send("it works ");
+app.get("/", async (req, res) => {
+  const allManga = await db.getAllManga();
+  res.render("home", { manga: allManga, links: data.links });
 });
 
 app.use(mangaRouter);
 app.use(authorRouter);
 app.use(publisherRouter);
 
-app.listen(3000, (req, res) => {
-  console.log("Server running on port 3000");
+app.listen(4000, (req, res) => {
+  console.log("Server running on port 4000");
 });
