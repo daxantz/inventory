0aa48a3b4ccf27ccb84f0308edecdf09162ff44f
daxantz <daxantz@gmail.com>
2024-09-16 15:21:16 -0400

    changed url pattern for getting form to a more suitable name

--- src/routes/authorRouter.js
+++ src/routes/authorRouter.js
@@ -4,7 +4,7 @@ const authorRouter = Router();
 
 authorRouter.get("/authors", authorController.authorListGet); // get all author
 authorRouter.get("/authors/:id", authorController.authorGet); // get specific author
-authorRouter.get("/author", authorController.authorCreateGet); //get the author creation form
+authorRouter.get("/add-author", authorController.authorCreateGet); //get the author creation form
 authorRouter.post("/author", authorController.authorCreatePost); // create a new author entry
 // authorRouter.put("/author/:id", authorController.authorUpdatePut); // update a single author
 // authorRouter.delete("/author/:id", authorController.authorDelete); // delete a single author
